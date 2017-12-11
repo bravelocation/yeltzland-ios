3bba449fc0ce048ed7b8e825a2b17bc520648664
John Pollard <johnp@bravelocation.com>
2017-12-11 10:55:52 +0000

    Initial code for TV OS app

---
+++ yeltzland TVOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yeltzland TVOS
+//
+//  Created by John Pollard on 11/12/2017.
+//  Copyright © 2017 John Pollard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
