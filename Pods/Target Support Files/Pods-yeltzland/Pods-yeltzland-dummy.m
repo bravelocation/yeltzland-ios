39c7ebe4e266a50bf8e3a5736e64ec7c5cce6c47
John Pollard <johnp@bravelocation.com>
2018-09-10 15:31:48 +0100

    Removing Pods from repository

--- Pods/Target Support Files/Pods-yeltzland/Pods-yeltzland-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_yeltzland : NSObject
-@end
-@implementation PodsDummy_Pods_yeltzland
-@end
--- Pods/Target Support Files/Pods-yeltzlandTVOS/Pods-yeltzlandTVOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_yeltzlandTVOS : NSObject
-@end
-@implementation PodsDummy_Pods_yeltzlandTVOS
-@end
