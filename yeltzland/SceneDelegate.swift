e8bdaea9f4d68a853832ff9c324b0a78e17d60b4
John Pollard <johnp@bravelocation.com>
2019-07-11 09:06:51 +0100

    Added shortcut support for scene delegate

--- yeltzland/SceneDelegate.swift
+++ yeltzland/SceneDelegate.swift
@@ -41,4 +41,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window.rootViewController?.restoreUserActivityState(userActivity)
         }
     }
+    
+    @available(iOS 13.0, *)
+    func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        print("3D Touch when from shortcut action")
+        let startingActivity = NSUserActivity(activityType: "com.bravelocation.yeltzland.currenttab")
+        startingActivity.userInfo = [:]
+        startingActivity.userInfo?["com.bravelocation.yeltzland.currenttab.key"] = self.handleShortcut(shortcutItem)
+        
+        // Reset selected tab
+        if let mainViewController = self.window?.rootViewController as? MainTabBarController {
+            mainViewController.restoreUserActivityState(startingActivity)
+        }
+        
+        return completionHandler(true)
+    }
+    
+    // MARK: - Private functions
+    func handleShortcut(_ shortcutItem: UIApplicationShortcutItem) -> Int {
+        print("Handling shortcut item %@", shortcutItem.type)
+        
+        switch shortcutItem.type {
+        case "com.bravelocation.yeltzland.forum":
+            return 0
+        case "com.bravelocation.yeltzland.official":
+            return 1
+        case "com.bravelocation.yeltzland.yeltztv":
+            return 2
+        case "com.bravelocation.yeltzland.twitter":
+            return 3
+        default:
+            return 0
+        }
+    }
 }
