6d99edf530b429593ae096c34ff63102b67d70bd
John Pollard <johnp@bravelocation.com>
2020-07-27 13:17:35 +0100

    Added max width to Twitter timeline

--- yeltzland/Views/TwitterTimelineView.swift
+++ yeltzland/Views/TwitterTimelineView.swift
@@ -24,6 +24,7 @@ struct TwitterTimelineView: View {
                 }
             }
         }
+        .frame(maxWidth: 800)
         .onAppear {
             self.tweetData.refreshData()
         }
