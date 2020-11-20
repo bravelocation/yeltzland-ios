e56bf253d6974deef192b19f712e40abb21d54e6
John Pollard <johnp@bravelocation.com>
2020-11-20 12:51:25 +0000

    Initial refactoring into custom Twitter hosting controller

---
+++ yeltzland/TwitterHostingController.swift
@@ -0,0 +1,39 @@
+//
+//  TwitterHostingController.swift
+//  Yeltzland
+//
+//  Created by John Pollard on 20/11/2020.
+//  Copyright © 2020 John Pollard. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+class TwitterHostingController<Content: View>: UIHostingController<Content> {
+
+    let tweetData: TweetData
+
+    init(twitterAccountName: String) {
+        
+        let twitterConsumerKey = SettingsManager.shared.getSetting("TwitterConsumerKey") as! String
+        let twitterConsumerSecret = SettingsManager.shared.getSetting("TwitterConsumerSecret") as! String
+        
+        let twitterDataProvider = TwitterDataProvider(
+            twitterConsumerKey: twitterConsumerKey,
+            twitterConsumerSecret: twitterConsumerSecret,
+            tweetCount: 20,
+            accountName: twitterAccountName
+        )
+        
+        self.tweetData = TweetData(dataProvider: twitterDataProvider, accountName: twitterAccountName)
+        
+        let rootView = AnyView(TwitterView().environmentObject(tweetData))
+
+        super.init(rootView: rootView as! Content)
+    }
+    
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
